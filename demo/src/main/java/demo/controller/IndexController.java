bb5d224d86453b9b394810f3a6d2b1411920fc1c
PGoliveira21 <paulooak7@gmail.com>
2024-01-01 12:32:08 -0300

    passando parametros via url para o metodo

--- demo/src/main/java/demo/controller/IndexController.java
+++ demo/src/main/java/demo/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //arquitetura rest
@@ -12,8 +13,9 @@ public class IndexController {
 
 	//ja e um serviço restfull
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity ("Ola REST SPRING" , HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "nome", required = false) String nome) {
+		System.out.println("parametro passado " + nome);
+		return new ResponseEntity ("Ola REST SPRING " + nome, HttpStatus.OK);
 	}
 	
 	
