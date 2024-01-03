e803bca58920d67c592588f8bc92383665a73963
PGoliveira21 <paulooak7@gmail.com>
2024-01-02 21:02:14 -0300

    consultando usuarios no banco de dados usando id passado na url e consultando todos retornando uma List

--- demo/src/main/java/demo/controller/IndexController.java
+++ demo/src/main/java/demo/controller/IndexController.java
@@ -1,45 +1,49 @@
 package demo.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.model.Usuario;
+import demo.repository.UsuarioRepository;
 
 @RestController //arquitetura rest
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	
 	//ja e um serviço restfull
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id")Long id) {
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(1L);
-		usuario.setNome("Paulo");
-		usuario.setLogin("PGO");
-		usuario.setSenha("12345678");
-		
-
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(12L);
-		usuario2.setNome("ana");
-		usuario2.setLogin("aGO");
-		usuario2.setSenha("12345678");
-		
-		List<Usuario> usuarios = new ArrayList<>();
-		usuarios.add(usuario2);
-		usuarios.add(usuario);
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
  	
-		return new ResponseEntity(usuarios, HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuarios (){
+		
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	
 	
 	
 }
