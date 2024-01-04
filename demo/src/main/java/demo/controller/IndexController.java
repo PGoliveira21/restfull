5aba8afec6727f908f25f7365367cd0225d09c71
PGoliveira21 <paulooak7@gmail.com>
2024-01-04 20:23:25 -0300

    criando um serviço de post no banco de dados e criando uma segunda URL simulando um serviço de post de venda

--- demo/src/main/java/demo/controller/IndexController.java
+++ demo/src/main/java/demo/controller/IndexController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +54,22 @@ public class IndexController {
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 	
+	@PostMapping(value = "/" , produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
 	
-	
+	@PostMapping(value = "/{iduser}/venda/{idvenda}" , produces = "application/json")
+	public ResponseEntity cadastrar(@PathVariable Long iduser, @PathVariable Long idvenda){
+		
+	//	Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("concluido com sucesso" + iduser + +idvenda, HttpStatus.OK);
+		
+	}
 	
 }
