7cfbd2b69f8853a096abd09163ce4e2bf394b628
PGoliveira21 <paulooak7@gmail.com>
2024-01-07 17:06:49 -0300

    metodo de deletar criado recebendo uma requisição com o parametro do id e apagando dados no banco de dados e retornando uma mensagem de deletado

--- demo/src/main/java/demo/controller/IndexController.java
+++ demo/src/main/java/demo/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -82,6 +83,14 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		
+	}
+	
+	@DeleteMapping(value = "/{iduser}" , produces = "application/text")
+	public String deletarusuario(@PathVariable Long iduser) {
+		
+		usuarioRepository.deleteById(iduser);
+		
+		return "Deletado";
 	}
 	
 }
